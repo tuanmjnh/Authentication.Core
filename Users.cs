7d30e255e093adc7b028bd6bb02aeb372eb6e988
Tuấn Hin <minhtuan200990@gmail.com>
2018-05-25 14:55:28 +0700

    tmvnpt

--- Users.cs
+++ Users.cs
@@ -3,45 +3,61 @@
     using System.ComponentModel.DataAnnotations;
     using System;
 
-    [Table ("Users")]
+    [Table("Users")]
     public partial class Users {
         [Dapper.Contrib.Extensions.ExplicitKey]
-        public Guid id { get; set; }
+        public string UserID { get; set; }
 
-        [StringLength (128)]
+        [StringLength(128)]
+        public string parentId { get; set; }
+
+        [StringLength(512)]
+        public string groupId { get; set; }
+
+        [StringLength(128)]
         public string username { get; set; }
 
-        [StringLength (256)]
+        [StringLength(256)]
         public string password { get; set; }
 
-        [StringLength (128)]
+        [StringLength(128)]
         public string salt { get; set; }
 
-        [StringLength (256)]
-        public string full_name { get; set; }
+        [StringLength(256)]
+        public string fullname { get; set; }
 
-        [StringLength (128)]
+        [StringLength(128)]
         public string mobile { get; set; }
 
-        [StringLength (256)]
+        [StringLength(256)]
         public string email { get; set; }
+
+        [StringLength(1024)]
         public string address { get; set; }
+
+        [StringLength(2048)]
+        public string details { get; set; }
+
+        [StringLength(512)]
+        public string images { get; set; }
+        public int orders { get; set; }
+
+        [StringLength(2048)]
         public string roles { get; set; }
 
-        [StringLength (128)]
-        public string created_by { get; set; }
-        public DateTime created_at { get; set; }
+        [StringLength(128)]
+        public string createdBy { get; set; }
+        public DateTime? createdAt { get; set; }
 
-        [StringLength (128)]
-        public string updated_by { get; set; }
-        public DateTime? updated_at { get; set; }
-        public DateTime? last_login { get; set; }
+        [StringLength(128)]
+        public string updatedBy { get; set; }
+        public DateTime? updatedAt { get; set; }
 
-        [StringLength (256)]
-        public string staff_id2 { get; set; }
-        public Guid staff_id { get; set; }
-        public string group_id { get; set; }
-        public string user_id { get; set; }
+        [StringLength(128)]
+        public string deletedBy { get; set; }
+        public DateTime? deletedAt { get; set; }
+        public DateTime? lastlogin { get; set; }
+        public DateTime? lastChangePassword { get; set; }
         public int flag { get; set; }
         public string extras { get; set; }
     }
